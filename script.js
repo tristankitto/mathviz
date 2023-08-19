33d2f692439431212f256cb617da5671cfe8c1b5
Tristan Kitto <kittr801@student.otago.ac.nz>
2023-08-19 14:43:13 +1200

    Created simple, interactive sine curve

---
+++ script.js
@@ -0,0 +1,64 @@
+let xOff = 0;
+let xIncrement = 2;
+const amplitudeSlider = document.getElementById('amplitude');
+const wavelengthSlider = document.getElementById('wavelength');
+const canvasContainer = document.getElementById('canvas-container');
+const canvasWidth = canvasContainer.clientWidth;
+const canvasHeight = 300;
+
+let dotPositions = [];
+
+function setup() {
+    const canvas = createCanvas(canvasWidth, canvasHeight);
+    canvas.parent('canvas-container');
+}
+
+function draw() {
+    const yAmplitude = amplitudeSlider.value;
+    const period = wavelengthSlider.value;
+
+    if (xOff >= canvasWidth) {
+        clearCanvas();
+    }
+
+    background(255);
+
+    translate(0, height / 2);
+
+    const x = xOff % canvasWidth;
+    const y = yAmplitude * sin(TWO_PI * xOff / period);
+
+    noStroke();
+    fill(0);
+    ellipse(x, y, 8);
+
+    dotPositions.push({ x, y });
+
+    stroke(0);
+    noFill();
+    beginShape();
+    for (const pos of dotPositions) {
+        vertex(pos.x, pos.y);
+    }
+    endShape();
+
+    if (dotPositions.length > canvasWidth) {
+        dotPositions.shift();
+    }
+
+    xOff += xIncrement;
+}
+
+amplitudeSlider.addEventListener('input', function () {
+    clearCanvas();
+});
+
+wavelengthSlider.addEventListener('input', function () {
+    clearCanvas();
+});
+
+function clearCanvas() {
+    clear();
+    dotPositions = [];
+    xOff = 0;
+}
